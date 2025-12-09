9e7090c5f5afe18393dd337545ba3e04c43db905
Abhinav Rai <122885696+AbhinavJD7@users.noreply.github.com>
2025-12-09 17:24:51 +0530

    White-Label to HBMP AgentBot with custom branding

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -58,8 +58,8 @@ export default defineConfig(({ command }) => ({
       },
       includeAssets: [],
       manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
+        name: 'HBMP AgentBot',
+        short_name: 'HBMP AgentBot',
         display: 'standalone',
         background_color: '#000000',
         theme_color: '#009688',
